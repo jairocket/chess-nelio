3ae08f980c9ec5f2d6877cc12097f51ccacc18bd
Jailson Anjos <jailsonanjos@hotmail.com>
2023-10-13 12:02:35 -0300

    feat(piece): Add Bishop

--- src/main/java/org/chess/chess/ChessMatch.java
+++ src/main/java/org/chess/chess/ChessMatch.java
@@ -5,6 +5,7 @@ import org.chess.boardgame.Piece;
 import org.chess.boardgame.Position;
 import org.chess.chess.enums.Color;
 import org.chess.chess.exceptions.ChessException;
+import org.chess.chess.pieces.Bishop;
 import org.chess.chess.pieces.King;
 import org.chess.chess.pieces.Pawn;
 import org.chess.chess.pieces.Rook;
@@ -190,7 +191,11 @@ public class ChessMatch {
 
     private void initialSetup() {
         placeNewPiece('a', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
+
         placeNewPiece('e', 1, new King(board, Color.WHITE));
+        placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
+
         placeNewPiece('h', 1, new Rook(board, Color.WHITE));
 
         placeNewPiece('a', 2, new Pawn(board, Color.WHITE));
@@ -204,7 +209,9 @@ public class ChessMatch {
 
 
         placeNewPiece('a', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
         placeNewPiece('e', 8, new King(board, Color.BLACK));
+        placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
         placeNewPiece('h', 8, new Rook(board, Color.BLACK));
 
         placeNewPiece('a', 7, new Pawn(board, Color.BLACK));
